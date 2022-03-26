97fb4d969fad3ef6ee1670b60b2dcb7295eb2aef
André Vitor de Freitas Adams <55411040+andrevitor103@users.noreply.github.com>
2022-03-25 22:31:48 -0300

    Create exemplo_conversao.py

---
+++ exemplo_conversao.py
@@ -0,0 +1,11 @@
+import cv2
+#criar pasta imagem e colocar imagem
+imagem = cv2.imread("imagens/watch.png")
+imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+imagem_hsv = cv2.hsvColor(imagem, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Imagem cinza", imagem_cinza)
+cv2.waitkey(0)
+
+cv2.imshow("Imagem hsv", imagem_hsv)
+cv2.waitkey(0)
